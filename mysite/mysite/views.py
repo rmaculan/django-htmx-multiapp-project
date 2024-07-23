75e988aab3be1f2a1927c7f02d006473add54cde
Robert <robertmaculan@gmail.com>
2024-07-22 22:25:11 -0400

    UI updates

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
+from marketplace.models import Item
 
 def landing_page(request):
-    return render(request, 'landing_page.html')
+    blog_posts = Post.objects.all()[:5]  # Fetch the latest 5 blog posts
+    marketplace_items = Item.objects.all()[:5]  # Fetch the latest 5 marketplace items
+    
+    return render(request, 'landing_page.html', {
+        'blog_posts': blog_posts,
+        'marketplace_items': marketplace_items,
+    })
